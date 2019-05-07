a4e9ff50079b54c020e3cfdf1b46ecd4c6c32f0e
mahdi312 <mahdi_mostafavi2011@yahoo.com>
2019-05-07 18:04:38 +0430

    first Commit

--- Content/190669.js
+++ Content/190669.js
@@ -29,8 +29,54 @@ $scope.makeDesign_293354 = function(param){
 
 $scope.makeDesign_293355 = function(param){ 
 	 $rootScope.design_10111($scope,param); 
-} 
- 
+}
+
+
+    $(function() {
+        'use strict';
+
+        var body = $('body');
+
+        function goToNextInput(e) {
+            var key = e.which,
+                t = $(e.target),
+                sib = t.next('input');
+
+            if (key != 9 && (key < 48 || key > 57)) {
+                e.preventDefault();
+                return false;
+            }
+
+            if (key === 9) {
+                return true;
+            }
+
+            if (!sib || !sib.length) {
+                sib = body.find('input').eq(0);
+            }
+            sib.select().focus();
+        }
+
+        function onKeyDown(e) {
+            var key = e.which;
+
+            if (key === 9 || (key >= 48 && key <= 57)) {
+                return true;
+            }
+
+            e.preventDefault();
+            return false;
+        }
+
+        function onFocus(e) {
+            $(e.target).select();
+        }
+
+        body.on('keyup', 'input', goToNextInput);
+        body.on('keydown', 'input', onKeyDown);
+        body.on('click', 'input', onFocus);
+
+    });
 
  
  
@@ -47,6 +93,7 @@ $scope.makeDesign_293355 = function(param){
 		timer();
 	}
 }]);
+
 app.requires.push('ngMaterial','ngStorage','oc.lazyLoad','pascalprecht.translate','ngMessages','ngMask');
 
  app.directive('validNumber', function() {
